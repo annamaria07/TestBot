a5495c55bb5a91496060c10e61075d2d8e7ddf03
annamaria07 <98493485+annamaria07@users.noreply.github.com>
2022-01-26 17:46:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def msg_received_from_group():
   log('{}'.format(data))
   
   #Check the text of the message sent to the chat to see if it matches our command word
-  if data['text'].lower() == "!hello":
+  if data['text'].lower() == "hello":
     send_msg("hello there everbody my name is markiplier")
 	
 
